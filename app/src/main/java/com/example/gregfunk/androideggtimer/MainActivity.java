485284a2e8142ab22181b1ac2accb864f7898037
Greg Funk <greg.funk@walltowall.com>
2015-11-16 14:19:30 -0500

    Seekbar makes time change

--- app/src/main/java/com/example/gregfunk/androideggtimer/MainActivity.java
+++ app/src/main/java/com/example/gregfunk/androideggtimer/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.gregfunk.androideggtimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekBar = (SeekBar)findViewById(R.id.timerSeekBar);
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int minutes = (int) progress / 60;
+                int seconds = progress - (minutes * 60);
+                String secondString = Integer.toString(seconds);
+                if (secondString == "0") {
+                    secondString = "00";
+                }
+
+                TextView timerTextView = (TextView) findViewById(R.id.timerTextView);
+                timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
